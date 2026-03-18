09058a6ccc82cfd2ddd099c7c7a9c14d565bf03a
davidsanchez001 <david.sanchez9301@gmail.com>
2026-03-18 14:26:33 -0500

    Add files via upload

---
+++ Segundo-Corte/reto.sql
@@ -0,0 +1,37 @@
+# borrar
+# drop table (nombre de la tabla)
+# drop data base (nombre de la base de datos)
+drop table automovil;
+truncate table automovil,
+drop database companiaseguros;
+
+
+#retos
+#1
+create database tienda_online;
+use tienda_online
+
+#2
+create table productos
+create id producto
+truncate principal_productos
+name producto varchar (50)
+id varchar (10) not null
+create or hour 
+#fecha actual (currenttimestap)
+
+#3
+create table idcliente, add name, add@, number 
+restriction varchar (20) not null
+numero de telefono number double not null
+create table idpedido;
+create idclienteFK;
+create datatime;
+create total table 
+create kay from "pedidos" y "cliente"
+
+#4
+drop table add productos varchar (50)
+drop table truncate number movil varchar (50)
+drop table productos delie columna fecha_de_creacion
+drop rename table total from to monto_total
